da669cfe5c8f0e224eb224ac25a1b4210989827a
ajouatom <ajouatom@gmail.com>
2024-06-04 18:43:11 +0900

    test

--- selfdrive/ui/qt/home.cc
+++ selfdrive/ui/qt/home.cc
@@ -110,6 +110,10 @@ void HomeWindow::mousePressEvent(QMouseEvent* e) {
   if ((onroad->isVisible() || body->isVisible()) && (!sidebar->isVisible() || e->x() > sidebar->width())) {
     sidebar->setVisible(!sidebar->isVisible() && !onroad->isMapVisible());
   }
+
+  UIState* s = uiState();
+  s->show_brightness_timer = 20;
+  printf("mousePressEvent\n");
 }
 
 void HomeWindow::mouseDoubleClickEvent(QMouseEvent* e) {
