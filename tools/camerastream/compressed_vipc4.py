e8d61690cfc2c5907b13d8a287aa9f514a0a5bab
ajouatom <ajouatom@gmail.com>
2024-06-07 22:38:25 +0900

    ff

--- tools/camerastream/compressed_vipc4.py
+++ tools/camerastream/compressed_vipc4.py
@@ -29,25 +29,6 @@ def run_yolov8_on_frame(model, frame):
     return results
 
 def frame_processor(frame_queue, yolov8_model, debug=False):
-    import cv2
-    import numpy as np
-
-    # 이미지의 크기 설정 (예: 512x512)
-    height = 512
-    width = 512
-
-    # 랜덤 이미지 생성 (0에서 255 사이의 값으로 채워진 height x width x 3 크기의 배열)
-    random_image = np.random.randint(0, 256, (height, width, 3), dtype=np.uint8)
-
-    # 이미지 표시
-    cv2.imshow('Random Image', random_image)
-
-    # 아무 키나 누를 때까지 대기
-    cv2.waitKey(0)
-
-    # 창 닫기
-    cv2.destroyAllWindows()
-    return
     while True:
         frame = frame_queue.get()
         if frame is None:
@@ -180,6 +161,25 @@ class CompressedVipc:
     self.join()
 
 def main():
+  import cv2
+  import numpy as np
+
+  # 이미지의 크기 설정 (예: 512x512)
+  height = 512
+  width = 512
+
+  # 랜덤 이미지 생성 (0에서 255 사이의 값으로 채워진 height x width x 3 크기의 배열)
+  random_image = np.random.randint(0, 256, (height, width, 3), dtype=np.uint8)
+
+  # 이미지 표시
+  cv2.imshow('Random Image', random_image)
+
+  # 아무 키나 누를 때까지 대기
+  cv2.waitKey(0)
+
+  # 창 닫기
+  cv2.destroyAllWindows()
+  return
   frame_queue = Queue()
   yolov8_model = load_yolov8_model()  # Load YOLOv8 model once and pass it to decoder
   frame_processor(frame_queue, yolov8_model, debug=True)
