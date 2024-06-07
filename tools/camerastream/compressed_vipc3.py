85f7d3b6af0b4b3bd0bff49ae774dc4bf0c576af
ajouatom <ajouatom@gmail.com>
2024-06-07 20:47:43 +0900

    ff

--- tools/camerastream/compressed_vipc3.py
+++ tools/camerastream/compressed_vipc3.py
@@ -1,127 +1,73 @@
 #!/usr/bin/env python3
-import av
 import os
 import sys
 import numpy as np
+import cv2
 import multiprocessing
 import time
-import cv2
-
 import cereal.messaging as messaging
-from cereal.visionipc import VisionIpcServer, VisionStreamType
+from cereal.visionipc import VisionStreamType, VisionIpcClient
 
 V4L2_BUF_FLAG_KEYFRAME = 8
 
-ENCODE_SOCKETS = {
-  VisionStreamType.VISION_STREAM_ROAD: "roadEncodeData",
-}
-
-def decoder(addr, vipc_server, vst, W, H, debug=False):
-  sock_name = ENCODE_SOCKETS[vst]
-  if debug:
-    print(f"start decoder for {sock_name}, {W}x{H}")
-
-  codec = av.CodecContext.create("hevc", "r")
-
-  os.environ["ZMQ"] = "1"
-  messaging.context = messaging.Context()
-  sock = messaging.sub_sock(sock_name, None, addr=addr, conflate=False)
-  cnt = 0
-  last_idx = -1
-  seen_iframe = False
-
-  time_q = []
-  while 1:
-    msgs = messaging.drain_sock(sock, wait_for_one=True)
-    for evt in msgs:
-      evta = getattr(evt, evt.which())
-      if debug and evta.idx.encodeId != 0 and evta.idx.encodeId != (last_idx+1):
-        print("DROP PACKET!")
-      last_idx = evta.idx.encodeId
-      if not seen_iframe and not (evta.idx.flags & V4L2_BUF_FLAG_KEYFRAME):
-        if debug:
-          print("waiting for iframe")
-        continue
-      time_q.append(time.monotonic())
-      network_latency = (int(time.time()*1e9) - evta.unixTimestampNanos)/1e6
-      frame_latency = ((evta.idx.timestampEof/1e9) - (evta.idx.timestampSof/1e9))*1000
-      process_latency = ((evt.logMonoTime/1e9) - (evta.idx.timestampEof/1e9))*1000
-
-      # put in header (first)
-      if not seen_iframe:
-        codec.decode(av.packet.Packet(evta.header))
-        seen_iframe = True
-
-      frames = codec.decode(av.packet.Packet(evta.data))
-      if len(frames) == 0:
-        if debug:
-          print("DROP SURFACE")
-        continue
-      assert len(frames) == 1
-      img_yuv = frames[0].to_ndarray(format=av.video.format.VideoFormat('yuv420p')).flatten()
-      uv_offset = H*W
-      y = img_yuv[:uv_offset]
-      uv = img_yuv[uv_offset:].reshape(2, -1).ravel('F')
-      img_yuv = np.hstack((y, uv))
-
-      vipc_server.send(vst, img_yuv.data, cnt, int(time_q[0]*1e9), int(time.monotonic()*1e9))
-      cnt += 1
-
-      # Convert YUV to BGR for displaying with cv2
-      img_bgr = frames[0].to_ndarray(format='bgr24')
-      #cv2.imshow("Video Stream", img_bgr)
-      #if cv2.waitKey(1) & 0xFF == ord('q'):
-      #  break
-
-      pc_latency = (time.monotonic()-time_q[0])*1000
-      time_q = time_q[1:]
-      if debug:
-        print("%2d %4d %.3f %.3f roll %6.2f ms latency %6.2f ms + %6.2f ms + %6.2f ms = %6.2f ms"
-              % (len(msgs), evta.idx.encodeId, evt.logMonoTime/1e9, evta.idx.timestampEof/1e6, frame_latency,
-                 process_latency, network_latency, pc_latency, process_latency+network_latency+pc_latency ), len(evta.data), sock_name)
-
-  cv2.destroyAllWindows()
-
-class CompressedVipc:
-  def __init__(self, addr, vision_streams, debug=False):
-    print("getting frame sizes")
-    os.environ["ZMQ"] = "1"
-    messaging.context = messaging.Context()
-    sm = messaging.SubMaster([ENCODE_SOCKETS[s] for s in vision_streams], addr=addr)
-    while min(sm.recv_frame.values()) == 0:
-      sm.update(100)
-    os.environ.pop("ZMQ")
-    messaging.context = messaging.Context()
-
-    self.vipc_server = VisionIpcServer("camerad")
-    for vst in vision_streams:
-      ed = sm[ENCODE_SOCKETS[vst]]
-      self.vipc_server.create_buffers(vst, 4, False, ed.width, ed.height)
-    self.vipc_server.start_listener()
-
-    self.procs = []
-    for vst in vision_streams:
-      ed = sm[ENCODE_SOCKETS[vst]]
-      p = multiprocessing.Process(target=decoder, args=(addr, self.vipc_server, vst, ed.width, ed.height, debug))
-      p.start()
-      self.procs.append(p)
-
-  def join(self):
-    for p in self.procs:
-      p.join()
-
-  def kill(self):
-    for p in self.procs:
-      p.terminate()
-    self.join()
+def video_display(vipc_addr, vision_stream_type, W, H, debug=False):
+    print("Starting video display...")
+    
+    # Initialize the VisionIPC client
+    vipc_client = VisionIpcClient("camerad", vision_stream_type)
+    if not vipc_client.connect(False):
+        print("Failed to connect to VisionIPC server")
+        return
+    
+    print("Connected to VisionIPC server")
+    
+    while True:
+        # Get the frame from the VisionIPC client
+        frame = vipc_client.recv()
+        if frame is None:
+            if debug:
+                print("No frame received")
+            continue
+        
+        img_yuv = np.frombuffer(frame.data, dtype=np.uint8)
+        y_size = W * H
+        uv_size = y_size // 2
+        
+        # Extract Y, U, and V planes
+        y = img_yuv[:y_size].reshape(H, W)
+        uv = img_yuv[y_size:].reshape(2, uv_size // 2).T.reshape(H // 2, W)
+        
+        # Stack the U and V planes
+        u = uv[:, :W // 2]
+        v = uv[:, W // 2:]
+        
+        # Resize U and V planes to match Y plane
+        u = cv2.resize(u, (W, H), interpolation=cv2.INTER_LINEAR)
+        v = cv2.resize(v, (W, H), interpolation=cv2.INTER_LINEAR)
+        
+        # Merge Y, U, and V planes into one YUV image
+        img_yuv = cv2.merge([y, u, v])
+        
+        # Convert YUV image to BGR
+        img_bgr = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
+        
+        # Display the image
+        cv2.imshow("Video Stream", img_bgr)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    vipc_client.close()
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-  addr = "192.168.0.28"
-  debug = True
+    addr = "192.168.0.28"
+    W = 1920  # Width of the video frame
+    H = 1080  # Height of the video frame
+    debug = False
 
-  vision_streams = [
-    VisionStreamType.VISION_STREAM_ROAD,
-  ]
+    vision_stream_type = VisionStreamType.VISION_STREAM_ROAD
 
-  cvipc = CompressedVipc(addr, vision_streams, debug=debug)
-  cvipc.join()
+    p = multiprocessing.Process(target=video_display, args=(addr, vision_stream_type, W, H, debug))
+    p.start()
+    p.join()
