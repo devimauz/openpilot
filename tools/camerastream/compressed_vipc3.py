98bc3617aad7c4389ce7719ed3fee3246efec4cd
ajouatom <ajouatom@gmail.com>
2024-06-07 20:21:52 +0900

    ff

--- tools/camerastream/compressed_vipc3.py
+++ tools/camerastream/compressed_vipc3.py
@@ -1,57 +1,132 @@
-import cv2
+#!/usr/bin/env python3
+import av
+import os
+import sys
 import numpy as np
+import multiprocessing
+import time
+import cv2
 
-# 빈 이미지 생성 (512x512 크기, 3채널 RGB, 배경은 흰색)
-image = np.ones((512, 512, 3), np.uint8) * 255
+import cereal.messaging as messaging
+from cereal.visionipc import VisionIpcServer, VisionStreamType
 
-# 직선 그리기
-start_point = (50, 50)
-end_point = (400, 50)
-color = (0, 0, 255)  # 빨간색
-thickness = 5
-cv2.line(image, start_point, end_point, color, thickness)
+V4L2_BUF_FLAG_KEYFRAME = 8
 
-# 직사각형 그리기
-top_left = (50, 100)
-bottom_right = (400, 200)
-color = (0, 255, 0)  # 초록색
-thickness = 3
-cv2.rectangle(image, top_left, bottom_right, color, thickness)
+# start encoderd
+# also start cereal messaging bridge
+# then run this "./compressed_vipc.py <ip>"
 
-# 원 그리기
-center = (256, 300)
-radius = 50
-color = (255, 0, 0)  # 파란색
-thickness = -1  # 채우기
-cv2.circle(image, center, radius, color, thickness)
+ENCODE_SOCKETS = {
+  VisionStreamType.VISION_STREAM_ROAD: "roadEncodeData",
+}
 
-# 타원 그리기
-center = (256, 400)
-axes = (100, 50)
-angle = 0
-startAngle = 0
-endAngle = 360
-color = (0, 255, 255)  # 노란색
-thickness = 2
-cv2.ellipse(image, center, axes, angle, startAngle, endAngle, color, thickness)
+def decoder(addr, vipc_server, vst, W, H, debug=False):
+  sock_name = ENCODE_SOCKETS[vst]
+  if debug:
+    print(f"start decoder for {sock_name}, {W}x{H}")
 
-# 다각형 그리기
-points = np.array([[100, 300], [200, 250], [300, 300], [200, 350]], np.int32)
-points = points.reshape((-1, 1, 2))
-color = (255, 0, 255)  # 자주색
-thickness = 2
-cv2.polylines(image, [points], isClosed=True, color=color, thickness=thickness)
+  codec = av.CodecContext.create("hevc", "r")
 
-# 텍스트 그리기
-text = "Hello OpenCV"
-org = (50, 450)
-font = cv2.FONT_HERSHEY_SIMPLEX
-fontScale = 1
-color = (0, 0, 0)  # 검은색
-thickness = 2
-cv2.putText(image, text, org, font, fontScale, color, thickness, cv2.LINE_AA)
+  os.environ["ZMQ"] = "1"
+  messaging.context = messaging.Context()
+  sock = messaging.sub_sock(sock_name, None, addr=addr, conflate=False)
+  cnt = 0
+  last_idx = -1
+  seen_iframe = False
 
-# 이미지 창 열기
-cv2.imshow('Drawn Shapes', image)
-cv2.waitKey(0)  # 키 입력을 기다림
-cv2.destroyAllWindows()  # 모든 창 닫기
+  time_q = []
+  while 1:
+    msgs = messaging.drain_sock(sock, wait_for_one=True)
+    for evt in msgs:
+      evta = getattr(evt, evt.which())
+      if debug and evta.idx.encodeId != 0 and evta.idx.encodeId != (last_idx+1):
+        print("DROP PACKET!")
+      last_idx = evta.idx.encodeId
+      if not seen_iframe and not (evta.idx.flags & V4L2_BUF_FLAG_KEYFRAME):
+        if debug:
+          print("waiting for iframe")
+        continue
+      time_q.append(time.monotonic())
+      network_latency = (int(time.time()*1e9) - evta.unixTimestampNanos)/1e6
+      frame_latency = ((evta.idx.timestampEof/1e9) - (evta.idx.timestampSof/1e9))*1000
+      process_latency = ((evt.logMonoTime/1e9) - (evta.idx.timestampEof/1e9))*1000
+
+      # put in header (first)
+      if not seen_iframe:
+        codec.decode(av.packet.Packet(evta.header))
+        seen_iframe = True
+
+      frames = codec.decode(av.packet.Packet(evta.data))
+      if len(frames) == 0:
+        if debug:
+          print("DROP SURFACE")
+        continue
+      assert len(frames) == 1
+      img_yuv = frames[0].to_ndarray(format=av.video.format.VideoFormat('yuv420p')).flatten()
+      uv_offset = H*W
+      y = img_yuv[:uv_offset]
+      uv = img_yuv[uv_offset:].reshape(2, -1).ravel('F')
+      img_yuv = np.hstack((y, uv))
+
+      vipc_server.send(vst, img_yuv.data, cnt, int(time_q[0]*1e9), int(time.monotonic()*1e9))
+      cnt += 1
+
+      # Convert YUV to BGR for displaying with cv2
+      img_bgr = frames[0].to_ndarray(format='bgr24')
+      cv2.imshow("Video Stream", img_bgr)
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+      pc_latency = (time.monotonic()-time_q[0])*1000
+      time_q = time_q[1:]
+      if debug:
+        print("%2d %4d %.3f %.3f roll %6.2f ms latency %6.2f ms + %6.2f ms + %6.2f ms = %6.2f ms"
+              % (len(msgs), evta.idx.encodeId, evt.logMonoTime/1e9, evta.idx.timestampEof/1e6, frame_latency,
+                 process_latency, network_latency, pc_latency, process_latency+network_latency+pc_latency ), len(evta.data), sock_name)
+  
+  cv2.destroyAllWindows()
+
+
+class CompressedVipc:
+  def __init__(self, addr, vision_streams, debug=False):
+    print("getting frame sizes")
+    os.environ["ZMQ"] = "1"
+    messaging.context = messaging.Context()
+    sm = messaging.SubMaster([ENCODE_SOCKETS[s] for s in vision_streams], addr=addr)
+    while min(sm.recv_frame.values()) == 0:
+      sm.update(100)
+    os.environ.pop("ZMQ")
+    messaging.context = messaging.Context()
+
+    self.vipc_server = VisionIpcServer("camerad")
+    for vst in vision_streams:
+      ed = sm[ENCODE_SOCKETS[vst]]
+      self.vipc_server.create_buffers(vst, 4, False, ed.width, ed.height)
+    self.vipc_server.start_listener()
+
+    self.procs = []
+    for vst in vision_streams:
+      ed = sm[ENCODE_SOCKETS[vst]]
+      p = multiprocessing.Process(target=decoder, args=(addr, self.vipc_server, vst, ed.width, ed.height, debug))
+      p.start()
+      self.procs.append(p)
+
+  def join(self):
+    for p in self.procs:
+      p.join()
+
+  def kill(self):
+    for p in self.procs:
+      p.terminate()
+    self.join()
+
+if __name__ == "__main__":
+  addr = "192.168.0.28"
+  debug = False
+
+  vision_streams = [
+    VisionStreamType.VISION_STREAM_ROAD,
+  ]
+
+  cvipc = CompressedVipc(addr, vision_streams, debug=debug)
+  cvipc.join()
