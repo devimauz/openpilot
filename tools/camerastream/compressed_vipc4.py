54ba9cb387f122466acab48e240464c81ef22474
ajouatom <ajouatom@gmail.com>
2024-06-08 07:31:40 +0900

    ff

--- tools/camerastream/compressed_vipc4.py
+++ tools/camerastream/compressed_vipc4.py
@@ -108,7 +108,7 @@ def decoder(addr, vipc_server, vst, W, H, frame_queue, debug=False):
 
       # Capture and display the frame every second
       current_time = time.time()
-      if current_time - last_capture_time >= 1:
+      if current_time - last_capture_time >= 0.2:
         img_rgb = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR_I420)
         frame_queue.put((img_rgb, cnt))
         last_capture_time = current_time
