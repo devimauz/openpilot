6b0321b5188d9d2937099c312728df34090b26e0
ajouatom <ajouatom@gmail.com>
2024-06-09 10:55:02 +0900

    ff

--- tools/camerastream/compressed_vipc4.py
+++ tools/camerastream/compressed_vipc4.py
@@ -76,7 +76,7 @@ def decoder(addr, vipc_server, vst, W, H, frame_queue, debug=False):
 
   os.environ["ZMQ"] = "1"
   messaging.context = messaging.Context()
-  sock = messaging.sub_sock(sock_name, None, addr=addr, conflate=False)
+  sock = messaging.sub_sock(sock_name, None, addr=addr, conflate=True)
   cnt = 0
   last_idx = -1
   seen_iframe = False
@@ -114,7 +114,7 @@ def decoder(addr, vipc_server, vst, W, H, frame_queue, debug=False):
 
       # Capture and display the frame every second
       current_time = time.time()
-      if current_time - last_capture_time >= 0.1 and frame_queue.qsize() < 1:
+      if current_time - last_capture_time >= 0.1 and frame_queue.qsize() < 3:
         img_rgb = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR_I420)
         frame_queue.put((img_rgb, cnt))
         last_capture_time = current_time
